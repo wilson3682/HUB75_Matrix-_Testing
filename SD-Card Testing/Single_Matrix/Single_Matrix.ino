2e1614bc774d7225a42c3e00377b822646d08f75
wilson3682 <willy8236@gmail.com>
2023-01-30 07:46:36 -0500

    Update Single_Matrix.ino

--- SD-Card Testing/Single_Matrix/Single_Matrix.ino
+++ SD-Card Testing/Single_Matrix/Single_Matrix.ino
@@ -59,34 +59,6 @@ MatrixPanel_I2S_DMA *dma_display = nullptr;
 #define OE_PIN 33
 #define CLK_PIN 15
 
-
-//=======================Configuration for my Testing Board =====================================
-//===============Micro SD Card Module Pinout=============
-// VCC = 5V
-// GND = GND
-
-//#define HSPI_MISO 16
-//#define HSPI_MOSI 32
-//#define HSPI_SCLK 21
-//#define HSPI_CS   17
-//
-////===================Led Panel Pinout====================
-//#define R1_PIN 25
-//#define G1_PIN 26
-//#define B1_PIN 27
-//#define R2_PIN 14
-//#define G2_PIN 12
-//#define B2_PIN 13
-//#define A_PIN 23
-//#define B_PIN 19
-//#define C_PIN 5
-//#define D_PIN 17
-//#define E_PIN -1 //18
-//#define LAT_PIN 4
-//#define OE_PIN 15
-//#define CLK_PIN 16
-//=========================================================================================
-
 // Configure for your panel(s) as appropriate!
 
 #define PANEL_RES_X 64 // Number of pixels width of each individual panel module.
